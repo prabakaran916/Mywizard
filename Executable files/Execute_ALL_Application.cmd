99ee838618b806aec45b14311da649593366af69
Ramakrishnan <p.h.ramakrishnan@accenture.com>
2018-12-27 17:16:31 +0530

    first commit

---
+++ Executable files/Execute_ALL_Application.cmd
@@ -0,0 +1,4 @@
+@echo off
+REM run the program
+call regedit /s "C:\Users\p.h.ramakrishnan\Documents\myWizard\Supported Files\reg.reg"
+call "C:\Users\p.h.ramakrishnan\Documents\myWizard\Executable files\Execute_MAS.cmd"
---
+++ Executable files/Execute_MAS.cmd
@@ -0,0 +1,3 @@
+@echo off
+REM run the program
+"C:\Program Files\Java\jre1.8.0_181\bin\java.exe" -jar "C:\Users\p.h.ramakrishnan\Documents\myWizard\Source files\FDS_MAS.jar"
